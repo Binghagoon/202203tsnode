4f898a7dce434098cbae23fa6d23a1adfeaca407
Binghagoon <foat3376@korea.ac.kr>
2022-04-01 12:42:37 +0900

    Make execute lambda

--- src/authentication/execute.ts
+++ src/authentication/execute.ts
@@ -7,14 +7,13 @@ import getUsers from "./get/users";
 import postUsers from "./post/users";
 import putUsers from "./put/users";
 
-const execute: Executable = async function (app, conn) {
-  return merge(
+const execute: Executable = async (app, conn) =>
+  merge(
     await signUp(app, conn),
     await signUpAllow(app, conn),
     await signIn(app, conn),
     await getUsers(app, conn),
     await postUsers(app, conn),
-    await putUsers(app, conn),
+    await putUsers(app, conn)
   );
-};
 export default execute;
