3be87206e1c8b94caf35bb1aa75729c4fdd8be2e
Binghagoon <foat3376@korea.ac.kr>
2022-04-01 18:04:36 +0900

    Just apply prettier

--- src/authentication/post/users.ts
+++ src/authentication/post/users.ts
@@ -5,26 +5,26 @@ import noSufficientArgumentError from "../../base_modules/not_sufficient_argumen
 import { OkPacketTypeGuard } from "../../base_modules/type_guards/query_results_type_guards";
 import { Executable } from "types/types";
 
-const execute :Executable=(app,conn)=>{
+const execute: Executable = (app, conn) => {
   const postUsers: RequestHandler = (req, res) =>
-  catchError(res, async () => {
-    const sql =
-      "INSERT INTO user (realname, username, email, phone) VALUES (?, ?, ?, ?);";
-    const arg = req.body;
-    const params = [arg.realname, arg.username, arg.email, arg.phone];
-    noSufficientArgumentError(params);
-    const results = await connWithPromise(conn, sql, params);
-    if (!OkPacketTypeGuard(results)) {
-      throw "Type mismatched";
-    }
-    res.status(200).send({
-      status: "success",
-      id: results.insertId,
+    catchError(res, async () => {
+      const sql =
+        "INSERT INTO user (realname, username, email, phone) VALUES (?, ?, ?, ?);";
+      const arg = req.body;
+      const params = [arg.realname, arg.username, arg.email, arg.phone];
+      noSufficientArgumentError(params);
+      const results = await connWithPromise(conn, sql, params);
+      if (!OkPacketTypeGuard(results)) {
+        throw "Type mismatched";
+      }
+      res.status(200).send({
+        status: "success",
+        id: results.insertId,
+      });
     });
-  });
   app.post("/auth/users", postUsers);
-  return{
-      post:["/auth/users"],
-  }
-}
+  return {
+    post: ["/auth/users"],
+  };
+};
 export default execute;
