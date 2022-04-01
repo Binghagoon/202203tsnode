972c3bfb77d9b86c695a4d415bfbdac62767d1ca
Binghagoon <foat3376@korea.ac.kr>
2022-04-01 11:17:22 +0900

    Add `yeobin`

--- src/uuid.ts
+++ src/uuid.ts
@@ -4,6 +4,7 @@ export const receiverToUuid: { [user: string]: string } = {
   minseong: sensitive.friends.elements[0].uuid,
   driver1: sensitive.friends.elements[1].uuid,
   driver2: sensitive.friends.elements[2].uuid,
+  yeobin: sensitive.friends.elements[3].uuid,
 };
 export const receiver: string[] = [];
 export const getReceiver = (): string[] => {
