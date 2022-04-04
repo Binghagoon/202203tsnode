764565a47ce3529028d64d340a2882c0d8b2f3ae
Binghagoon <foat3376@korea.ac.kr>
2022-04-04 19:23:52 +0900

    Add deluser

--- src/users/execute.ts
+++ src/users/execute.ts
@@ -1,9 +1,10 @@
-import merge from "src/base_modules/merge_path_object";
+import merge from "../base_modules/merge_path_object";
 import { Executable } from "types/types";
 import ufi from "./get/users_from_id";
 import uft from "./get/users_from_type";
 import putUsers from "./put/users";
 import getAllUsers from "./get/users";
+import delUsers from "./delete/users";
 
 const execute: Executable = async (app, conn) =>
   merge(
@@ -11,6 +12,7 @@ const execute: Executable = async (app, conn) =>
     await uft(app, conn),
     await putUsers(app, conn),
     await getAllUsers(app, conn),
+    await delUsers(app, conn)
   );
 
 export default execute;
